3bf01fe1a16df17db6c084ef174c019b6cf10e6f
roojzk <roojzk@gmail.com>
2025-05-09 13:21:45 -0400

    Update main_git.py

--- main_git.py
+++ main_git.py
@@ -1,8 +1,8 @@
 def in_autotests_we_trust(a, b):
     if a != b:
-        print('Test passed')
+        print('Test Failed')
     else:
-        print('Test failed')
+        print('Test Passed')
 
 in_autotests_we_trust(10, 10)
 
